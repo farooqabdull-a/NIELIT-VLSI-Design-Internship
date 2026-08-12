9e4cbecb06b00030fe949c9ccea48d96d61be69e
FAROOQ ABDULLA <farooqabdulla6118@gmail.com>
2026-08-12 22:52:14 +0530

    Delete Digital VLSI Design/01_Logic_Gates_and_MUX/02_Encoder_Comparator_Gray_Converter directory

--- Digital VLSI Design/01_Logic_Gates_and_MUX/02_Encoder_Comparator_Gray_Converter/rtl/binary_to_gray_3bit.v
+++
@@ -1,7 +0,0 @@
-module binary_to_gray_3bit (
-    input [2:0] binary,
-    output [2:0] gray);
-    assign gray[2] = binary[2];
-    assign gray[1] = binary[2] ^ binary[1];
-    assign gray[0] = binary[1] ^ binary[0];
-endmodule
