fce25df33e3a75cdcac6814f3fbc9efa264eb868
FAROOQ ABDULLA <farooqabdulla6118@gmail.com>
2026-08-12 23:00:46 +0530

    Delete 01_Logic_Gates_and_MUX directory

--- 01_Logic_Gates_and_MUX/rtl/mux21_behavioral_always.v
+++
@@ -1,9 +0,0 @@
-module mux21_behavioral_always (
-    input i0,
-    input i1,
-    input s,
-    output reg y);
-    always @(*) begin
-        y = (i0 & ~s) | (i1 & s);
-    end
-endmodule
--- 01_Logic_Gates_and_MUX/rtl/mux21_dataflow_assign.v
+++
@@ -1,7 +0,0 @@
-module mux21_dataflow_assign (
-    input i0,
-    input i1,
-    input s,
-    output y);
-    assign y = (i0 & ~s) | (i1 & s);
-endmodule
